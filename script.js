ffe5bc56be0dccf8a84372f992eb485ad5a42d01
Luckyluchie <lucie.legat@hotmail.fr>
2020-12-31 12:28:33 +0100

    add animation on scrolling

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ menuBtn.addEventListener('click', function(){
     mainContent.style.marginTop = '250px'
   }
 })
+
+// scroll animation
+let slideInRight = {
+  distance: '100%',
+  origin: 'right',
+}
+
+let slideInLeft = {
+  distance: '100%',
+  origin: 'left',
+}
+
+ScrollReveal().reveal('.symbols-front', slideInRight)
+ScrollReveal().reveal('.symbols-back', slideInLeft)
+ScrollReveal().reveal('.symbols-mobile', slideInRight)
+ScrollReveal().reveal('.symbols-other', slideInLeft)
